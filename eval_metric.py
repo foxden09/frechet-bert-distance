9b16d334b0a5ed0640beee02c0a2b6cca96cea81
Alexis Fox <40548173+foxden09@users.noreply.github.com>
2024-11-05 21:59:50 -0500

    Update eval_metric.py

--- eval_metric.py
+++ eval_metric.py
@@ -23,6 +23,8 @@ parser.add_argument('--sample_num', type=int, default=10, help='sample number of
 parser.add_argument('--model_type', type=str, default='', help='pretrained model type or path to pretrained model')
 parser.add_argument('--batch_size', type=int, default=32)
 parser.add_argument('--is_chinese', type=int, default=0, help='Is Chinese corpus or not')
+parser.add_argument('--use_tukey_trans', action='store_true', help='Use Tukey transformation if specified')
+
 args = parser.parse_args()
 
 def read_mt_data(args):
