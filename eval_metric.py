e1fb9682a9200d95740ce263f66b8c3837390443
Alexis Fox <40548173+foxden09@users.noreply.github.com>
2024-11-07 00:22:41 -0500

    Update eval_metric.py

--- eval_metric.py
+++ eval_metric.py
@@ -157,7 +157,7 @@ def eval_metric(args):
             mu1, sigma1 = get_statistics(target_querys, target_answers, tokenizer, 
                                          model, args.batch_size, use_cuda=True)
             for source_answers in source_answer_list:
-                print("source")#, len(source_querys), len(source_answers))
+                print("source", len(source_answers))#, len(source_querys), len(source_answers))
                 #mu2, sigma2 = get_statistics(source_querys, source_answers, tokenizer, 
                 #                             model, args.batch_size, use_cuda=True)
                 #score = calculate_frechet_distance(mu1, sigma1, mu2, sigma2)
