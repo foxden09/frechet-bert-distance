2966c8ce9803417631930678623f39e96520ebe0
Alexis Fox <40548173+foxden09@users.noreply.github.com>
2024-11-06 23:59:12 -0500

    Update eval_metric.py

--- eval_metric.py
+++ eval_metric.py
@@ -147,6 +147,7 @@ def eval_metric(args):
         target_querys, target_answers = [], []
         for query, answers in zip(querys, refs):
             for answer in answers:
+                print(query, answer)
                 target_querys.append(query)
                 target_answers.append(answer)
         tokenizer, model = get_model_configs(args.model_type, args.is_chinese)
